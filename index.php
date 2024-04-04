d7d8a25de6653815a63b4fdb7bcdb4ae5f379879
Anthony Kusuma <hi@anthonykusuma.com>
2024-04-04 22:44:01 +0800

    add: Timber and Twig

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
-<!DOCTYPE html>
+<?php
 
-<html>
-  <head>
-    <title><?php bloginfo('name'); ?></title>
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-  </head>
+use Timber\Timber;
 
-  <body>
-    <h1><?php bloginfo('name'); ?></h1>
-    <p><?php bloginfo('description'); ?></p>
-  </body>
-</html>
+$data = [
+  'title' => 'A Timber Tutorial',
+];
+
+Timber::render('index.twig', $data);
